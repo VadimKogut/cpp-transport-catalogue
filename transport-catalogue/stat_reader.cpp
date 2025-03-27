29be25e62e98ab3ed2b00f0a1ab71c4776671707
VadimKogut <115023922+VadimKogut@users.noreply.github.com>
2025-03-27 15:04:58 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,4 +1,4 @@
-// место для вашего кода#include "stat_reader.h"
+#include "stat_reader.h"
 #include "input_reader.h"
 #include <iostream>
 #include <vector>
@@ -6,48 +6,48 @@
 
 namespace transport {
 
-namespace stat {
+    namespace stat {
 
-void ParseAndPrintStat(const catalogue::TransportCatalogue& transport_catalogue, std::string_view request, std::ostream& output) {
-    size_t space_pos = request.find(' ');
-    if (space_pos == std::string_view::npos) {
-        output << "Invalid request format" << std::endl;
-        return;
-    }
-    if (request.substr(0, space_pos) == "Bus") {
-        std::string_view bus_name = request.substr(space_pos + 1); // Берём часть строки после пробела
+        void ParseAndPrintStat(const catalogue::TransportCatalogue& transport_catalogue, std::string_view request, std::ostream& output) {
+            size_t space_pos = request.find(' ');
+            if (space_pos == std::string_view::npos) {
+                output << "Invalid request format" << std::endl;
+                return;
+            }
+            if (request.substr(0, space_pos) == "Bus") {
+                std::string_view bus_name = request.substr(space_pos + 1); // Берём часть строки после пробела
 
-        const auto* bus = transport_catalogue.GetBus(bus_name);
-        if (bus) {
-            output << "Bus " << bus->name << ": " << bus->stops.size() << " stops on route, "
-                << bus->count_unic_stops << " unique stops, " << bus->len_route << " route length" << std::endl;
-        }
-        else {
-            output << "Bus " << bus_name << ": not found" << std::endl;
-        }
-    }
-    else {
-        std::string_view stop_name = request.substr(space_pos + 1); // Берём часть строки после пробела
-
-        const auto* stop = transport_catalogue.GetStop(stop_name);
-        if (stop) {
-            if (!stop->buses.empty()) {
-                output << "Stop " << stop->name << ": buses";
-                for (auto& bus : stop->buses) {
-                    output << " " << bus;
+                const auto* bus = transport_catalogue.GetBus(bus_name);
+                if (bus) {
+                    output << "Bus " << bus->name << ": " << bus->stops.size() << " stops on route, "
+                        << bus->count_unic_stops << " unique stops, " << bus->geo_route << " route length, " <<bus->geo_route/bus->len_route << " curvature" << std::endl;
                 }
-                output << std::endl;
+                else {
+                    output << "Bus " << bus_name << ": not found" << std::endl;
+                }
             }
             else {
-                output << "Stop " << stop_name << ": no buses" << std::endl;
+                std::string_view stop_name = request.substr(space_pos + 1); // Берём часть строки после пробела
+
+                const auto* stop = transport_catalogue.GetStop(stop_name);
+                if (stop) {
+                    if (!stop->buses.empty()) {
+                        output << "Stop " << stop->name << ": buses";
+                        for (auto& bus : stop->buses) {
+                            output << " " << bus;
+                        }
+                        output << std::endl;
+                    }
+                    else {
+                        output << "Stop " << stop_name << ": no buses" << std::endl;
+                    }
+                }
+                else {
+                    output << "Stop " << stop_name << ": not found" << std::endl;
+                }
             }
         }
-        else {
-            output << "Stop " << stop_name << ": not found" << std::endl;
-        }
-    }
-}
 
-}  // namespace stat
+    }  // namespace stat
 
 }  // namespace transport

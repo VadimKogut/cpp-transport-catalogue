ca8c2784afb94f3573e205f3a89275d7735252a5
VadimKogut <115023922+VadimKogut@users.noreply.github.com>
2025-04-09 14:27:20 +0300

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -2,8 +2,7 @@
 
 #include <cmath>
 
-constexpr double PI = 3.1415926535;
-constexpr int EARTH_RADIUS = 6'371'000;
+namespace geo {
 
 struct Coordinates {
     double lat;
@@ -16,13 +15,6 @@ struct Coordinates {
     }
 };
 
-inline double ComputeDistance(Coordinates from, Coordinates to) {
-    using namespace std;
-    if (from == to) {
-        return 0;
-    }
-    static const double dr = PI / 180.;
-    return acos(sin(from.lat * dr) * sin(to.lat * dr)
-        + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-        * EARTH_RADIUS;
-}
+double ComputeDistance(Coordinates from, Coordinates to);
+
+} // namespace geo

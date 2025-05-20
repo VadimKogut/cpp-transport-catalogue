45825ddaf19869f2aaf7e625ebcebe4e2e1754e4
VadimKogut <115023922+VadimKogut@users.noreply.github.com>
2025-05-20 12:59:35 +0300

    Update transport_router.h

--- transport-catalogue/transport_router.h
+++ transport-catalogue/transport_router.h
@@ -1,42 +1,44 @@
-#pragma once
+#pragma once 
 
-#include "router.h"
-#include "transport_catalogue.h"
-#include "graph.h"
+#include "router.h" 
+#include "transport_catalogue.h" 
+#include "graph.h" 
 
-#include <memory>
-#include <unordered_map>
+#include <memory> 
+#include <unordered_map> 
+#include <vector>
 
-namespace transport {
+namespace transport { 
 
-struct RouteEdgeInfo {
-    std::string bus_name;
-    int span_count;
-    double time;
-    std::string stop_name;
+struct RouteEdgeInfo { 
+    std::string bus_name; 
+    int span_count; 
+    double time; 
+    std::string stop_name; 
 };
 
-class Router {
-public:
-    Router() = default;
-    Router(int bus_wait_time, double bus_velocity);
-    
+struct RouteInfo {
+    double total_time;
+    std::vector<RouteEdgeInfo> edges;
+};
+
+class Router { 
+public: 
+    Router() = default; 
+    Router(const Catalogue& catalogue, int bus_wait_time, double bus_velocity); 
+     
+    std::optional<RouteInfo> FindRoute(std::string_view from, std::string_view to) const; 
+     
+private: 
     void BuildGraph(const Catalogue& catalogue);
-    std::optional<graph::Router<double>::RouteInfo> FindRoute(std::string_view from, std::string_view to) const;
     
-    const graph::DirectedWeightedGraph<double>& GetGraph() const;
-    const std::unordered_map<graph::EdgeId, RouteEdgeInfo>& GetEdgeInfo() const;
-    int GetBusWaitTime() const;
-    double GetBusVelocity() const;
+    int bus_wait_time_ = 0; 
+    double bus_velocity_ = 0.0; 
+     
+    graph::DirectedWeightedGraph<double> graph_; 
+    std::unique_ptr<graph::Router<double>> router_; 
+    std::unordered_map<graph::EdgeId, RouteEdgeInfo> edge_info_; 
+    std::unordered_map<std::string, graph::VertexId> stop_ids_; 
+}; 
 
-private:
-    int bus_wait_time_ = 0;
-    double bus_velocity_ = 0.0;
-    
-    graph::DirectedWeightedGraph<double> graph_;
-    std::unique_ptr<graph::Router<double>> router_;
-    std::unordered_map<graph::EdgeId, RouteEdgeInfo> edge_info_;
-    std::unordered_map<std::string, graph::VertexId> stop_ids_;
-};
-
-} // namespace transport
+} // namespace transport 
